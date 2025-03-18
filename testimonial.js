03ce1055ab3cf8f43568c57f6c2ac5643be759c2
QuynhTram200822 <ttqtram.20it3@vku.udn.vn>
2025-03-18 16:36:36 +0700

    review section

---
+++ testimonial.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  $(".testimonial-slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: false,
+    draggable: false,
+    dots: true,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 912,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
